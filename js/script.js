1533787bbb5f2c2381e93a17c88a4dcd2fd459ca
Mala <m.nimalasuriya@gmail.com>
2016-06-29 15:20:12 -0700

    add notes to script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// zoom animation
+// TODO add back zoom animation because it's fun!
+// ZOOM ANIMATION
   $(document).ready(function(){
     $('.img-zoom').hover(function() {
         $(this).addClass('transition');
     }, function() {
         $(this).removeClass('transition');
     });
-// add code for redirect o github or whatever page
-// add code for banner animations
   });
