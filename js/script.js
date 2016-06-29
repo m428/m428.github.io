0f450bfc0e0863a54ce263cbff1abceb1006ef64
Mala <m.nimalasuriya@gmail.com>
2016-06-29 15:10:11 -0700

    add all portfolio files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// zoom animation
+  $(document).ready(function(){
+    $('.img-zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+// add code for redirect o github or whatever page
+// add code for banner animations
+  });
