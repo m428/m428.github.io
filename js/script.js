3dc3d306b462e9c349726d1d030155ba02f974c6
Mala Nimalasuriya <malanimalasuriya@Malas-MacBook-Pro.local>
2017-03-19 20:34:28 -0700

    remove old content

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// TODO add back zoom animation because it's fun!
-// ZOOM ANIMATION
-  $(document).ready(function(){
-    $('.img-zoom').hover(function() {
-        $(this).addClass('transition');
-    }, function() {
-        $(this).removeClass('transition');
-    });
-  });
